c2644711db68ce15637193a2bab409c5b05cd3c3
Eric Barker <eric.j.barker24@gmail.com>
2024-09-15 14:52:24 -0600

    Adding the PieceMovesCalculator Super Class and BishopMovesCalculator subClass

---
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -0,0 +1,21 @@
+package chess;
+
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Concrete class for calculating moves of a bishop.
+ */
+public class BishopMovesCalculator extends PieceMovesCalculator{
+
+    @Override
+    public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position) {
+        Collection<ChessMove> moves = new ArrayList<>();
+
+        // Bishop move Logic
+
+        return moves;
+    }
+}
---
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -0,0 +1,21 @@
+package chess;
+
+import java.util.Collection;
+
+
+/**
+ * Abstract base class for calculating moves of different chess pieces.
+ */
+public abstract class PieceMovesCalculator {
+    /**
+     * Calculates all valid moves for a piece at the given position on the board.
+     *
+     * @param board The chessboard on which to calculate moves.
+     * @param position The current position of the piece.
+     * @return A collection of valid moves.
+     */
+    public abstract Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position);
+
+    // Default Constructor
+    public PieceMovesCalculator(){}
+}
