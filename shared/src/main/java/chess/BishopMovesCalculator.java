a5b4599917277906762a4abcce95b6fbdce3b9df
Eric Barker <eric.j.barker24@gmail.com>
2024-09-15 15:58:25 -0600

    Laying out psuedo logic for Bishop Moves

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -20,6 +20,29 @@ public class BishopMovesCalculator extends PieceMovesCalculator{
 
         // Bishop move Logic
 
+        // Define diagonal movement directions
+        int[][] directions = {
+                {1,1}, // Upper-right
+                {1,-1}, // Upper-left
+                {-1,1}, // Bottom-right
+                {-1,-1} // Bottom-left
+        };
+
+        // Iterate over each diagonal direction
+        for(int[] direction: directions){
+            // Start from the current position
+
+            // Move in direction within the board boundaries
+                // Update position in the current diagonal direction
+                // Is the new position out of bound?
+                // Is the square empty?
+                    // Add the position to the moves Collection
+                // There is a piece
+                // Is it an enemy piece?
+                    // Add ChessMove
+                // Stop moving further in this direction
+        }
+
 
         return moves;
     }
