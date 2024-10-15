eb24a2fde8b5c0abf39c5ae4f5f5c63384ad772e
Eric Barker <eric.j.barker24@gmail.com>
2024-10-14 23:42:05 -0600

    Added the Models

---
+++ server/src/main/java/Models/Game.java
@@ -0,0 +1,63 @@
+package Models;
+
+import chess.ChessGame;
+
+public class Game {
+    int gameID;
+    String whiteUsername;
+    String blackUsername;
+    String gameName;
+    ChessGame game;
+
+    public Game() {
+    }
+
+    public Game(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+        this.gameID = gameID;
+        this.whiteUsername = whiteUsername;
+        this.blackUsername = blackUsername;
+        this.gameName = gameName;
+        this.game = game;
+    }
+
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(int gameID) {
+        this.gameID = gameID;
+    }
+
+    public String getWhiteUsername() {
+        return whiteUsername;
+    }
+
+    public void setWhiteUsername(String whiteUsername) {
+        this.whiteUsername = whiteUsername;
+    }
+
+    public String getBlackUsername() {
+        return blackUsername;
+    }
+
+    public void setBlackUsername(String blackUsername) {
+        this.blackUsername = blackUsername;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public ChessGame getGame() {
+        return game;
+    }
+
+    public void setGame(ChessGame game) {
+        this.game = game;
+    }
+}
