ec3b5500a40b6d400a6976a471e3c133f7f50e14
Eric Barker <eric.j.barker24@gmail.com>
2024-10-21 17:10:02 -0600

    First draft on Register method

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -7,6 +7,8 @@ import exception.ResponseException;
 import model.Auth;
 import model.User;
 
+import java.util.UUID;
+
 public class UserService {
     private final UserDAO userDAO;
     private final AuthDAO authDAO;
@@ -17,10 +19,20 @@ public class UserService {
     }
 
     public Auth register(User user) throws ResponseException, DataAccessException {
-        return null;
+
+        if (userDAO.getUser(user.username()) != null) {
+            throw new ResponseException(403, "Error: User already exists");
+        }
+
+        userDAO.addUser(user);
+        String authToken = UUID.randomUUID().toString();
+        authDAO.addAuth(authToken, user.username());
+
+        return new Auth(authToken, user.username());
     }
 
     public Auth login(User user) throws ResponseException, DataAccessException {
+
         return null;
     }
 
