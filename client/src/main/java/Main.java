5e33004d99c7a031c247f788ec3b8d971802b057
Eric Barker <eric.j.barker24@gmail.com>
2024-11-13 13:31:48 -0700

    Have to run to class

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -5,4 +5,14 @@ public class Main {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
     }
+
+
+    private static void displayPreLoginHelp() {
+        System.out.println("\nPre-Login Help:");
+        System.out.println("---------------");
+        System.out.println("Help      : Displays this help information.");
+        System.out.println("Quit      : Exits the program.");
+        System.out.println("Login     : Prompts for username and password to log in to your account.");
+        System.out.println("Register  : Prompts for new account information to register and log in.\n");
+    }
 }
