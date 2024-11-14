8874c9ac9fccb4acb2dacc12b581faf7628f9770
Eric Barker <eric.j.barker24@gmail.com>
2024-11-13 17:41:02 -0700

    Displaying PreLoginHelp()

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -2,8 +2,11 @@ import chess.*;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+
+        if (args.length == 0) {
+            System.out.println("\nMust Provide arguments.");
+            displayPreLoginHelp();
+        }
     }
 
 
