bd2245cd84a50d68881130f3d724ce8a9da58417
Eric Barker <eric.j.barker24@gmail.com>
2024-11-16 21:25:59 -0700

    First Draft of ServerFacade

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,9 +1,12 @@
 package ui;
 
+import com.google.gson.Gson;
 import exception.ResponseException;
 import server.ServerFacade;
 import ui.websocket.NotificationHandler;
 import ui.websocket.WebSocketFacade;
+
+import java.util.Arrays;
 
 public class ChessClient {
     private final String serverUrl;
@@ -14,12 +17,16 @@ public class ChessClient {
 
 
     public ChessClient(String serverUrl, NotificationHandler notificationHandler) {
+        server = new ServerFacade(serverUrl);
         this.serverUrl = serverUrl;
         this.notificationHandler = notificationHandler;
     }
 
-    public void eval(String input) {
+    public String eval(String input) {
         try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
             switch (input.toLowerCase()) {
                 case "help":
                     displayHelp();
@@ -39,22 +46,50 @@ public class ChessClient {
         }
     }
 
+//    public String signIn(String... params) throws ResponseException {
+//        if(params.length >= 1){
+//            state = UserState.SIGNEDIN;
+//            visitorName = String.join("-", params);
+//            ws = new WebSocketFacade(serverUrl, notificationHandler);
+//            ws.enterPetShop(visitorName);
+//            return String.format("You signed in as %s.", visitorName);
+//        }
+//        throw new ResponseException(400, "Expected: <yourname>");
+//    }
+
+    public String list() throws ResponseException {
+        assertSignedIn();
+        var games = server.listGames();
+        var result = new StringBuilder();
+        var gson = new Gson();
+        for (var game : games) {
+            result.append(gson.toJson(game)).append('\n');
+        }
+        return result.toString();
+    }
+
     private String displayHelp() {
         if (state == UserState.SIGNEDOUT) {
             System.out.println("Available commands:");
-            System.out.println("  help      - Display this help message.");
-            System.out.println("  quit      - Exit the application.");
-            System.out.println("  login     - Log in to your account.");
-            System.out.println("  register  - Register as a new user.");
+            System.out.println("-help      - Display this help message.");
+            System.out.println("-quit      - Exit the application.");
+            System.out.println("-login     - Log in to your account.");
+            System.out.println("-register  - Register as a new user.");
         }
         return """
-                - create <name> - a game
-                - list - games
-                - join <ID> [WHITE|BLACK] - a game
-                - observe <ID> - a game
+                - create    <name>                  - a game
+                - list                              - games
+                - join      <ID>    [WHITE|BLACK]   - a game
+                - observe   <ID>                    - a game
                 - help
                 - logout
                 - quit
                 """;
+    }
+
+    private void assertSignedIn() throws ResponseException {
+        if (state == UserState.SIGNEDOUT) {
+            throw new ResponseException(400, "You must sign in");
+        }
     }
 }
