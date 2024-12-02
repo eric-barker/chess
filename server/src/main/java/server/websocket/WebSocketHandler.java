ebedd859ee6c5a263e05362952f863b3040a3baf
Eric Barker <eric.j.barker24@gmail.com>
2024-12-01 17:50:33 -0700

    Adding bones from petshop

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -3,14 +3,26 @@ package server.websocket;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import websocket.commands.UserGameCommand;
+import com.google.gson.Gson;
+import dataaccess.DataAccess;
+import exception.ResponseException;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.util.Timer;
 
 @websocket
 public class WebSocketHandler {
 
+    private final ConnectionManager connections = new ConnectionManager();
+
     @OnWebSocketMessage
     public void onMessage(Session session, String message) {
-        try{
-            UserGameCommand command = Serializer.fromJson(message, UserGameCommand.class);
+        try {
+            UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
 
             // Check AuthToken
             String username = getUsername(command.getAuthToken());
@@ -20,13 +32,31 @@ public class WebSocketHandler {
             switch (command.getCommandType()) {
                 case CONNECT -> connect(session, username, (ConnectCommand) command);
                 case MAKE_MOVE -> makeMove(session, username, (MakeMoveCommand) command);
-                case LEAVE -> leaveGame(session, username, (LeaveGameCommand) command);
+                case LEAVE -> leave(session, username, (LeaveGameCommand) command);
                 case RESIGN -> resign(session, username, (ResignCommand) command);
-            } catch (UnauthorizedException e){
-                sendMessage(session.getRemote(), new ErrorMessage("Error: unauthorized"));
-            } catch (Exception ex){
-                ex.printStackTrace();
-                sendMessage(session.getRemote(), new ErrorMessage("Error: " + ex.getMessage()));
             }
+        } catch (ResponseException e) {
+            sendMessage(session.getRemote(), new ErrorMessage("Error: unauthorized"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            sendMessage(session.getRemote(), new ErrorMessage("Error: " + ex.getMessage()));
+        }
     }
+
+    private void connect(String visitorName, Session session) {
+        System.out.println("Connect stub function");
+    }
+
+    private void makeMove(String visitorName, Session session) {
+        System.out.println("MakeMove stub function");
+    }
+
+    private void leave(String visitorName, Session session) {
+        System.out.println("Leave stub function");
+    }
+
+    private void resign(String visitorName, Session session) {
+        System.out.println("Resign stub function");
+    }
+
 }
